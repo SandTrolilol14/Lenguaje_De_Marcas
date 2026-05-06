bc689e43cce305cc964abe5f54a3f9a18261ccc5
Sandro Sanvicente Sánchez <sss0020@alu.medac.es>
2026-05-06 09:08:05 +0200

    Empezado 3.js

--- src/JavaScript3_3/1.js
+++ src/JavaScript3_3/1.js
@@ -1,3 +1,4 @@
+// 1
 const a = 20;
 const b = 6;
 console.log(`Suma = ${a + b}`);
@@ -5,6 +6,7 @@ console.log(`Resta = ${a - b}`);
 console.log(`Producto = ${a % b}`);
 console.log(`División = ${a / b}`);
 
+// 2
 const base = 8;
 const altura = 9;
 const area = base * altura;
@@ -12,39 +14,52 @@ const perimetro = 2 * (base + altura);
 console.log(`Área = ${area}`);
 console.log(`Perímetro = ${perimetro}`);
 
+// 3
 const nota1 = 7;
 const nota2 = 9;
 const nota3 = 6
 const media = nota1 + nota2 + nota3 / 3;
 console.log(media);
 
+// 4
 const celsius = 100;
 const fahrenheit = celsius * (9/5) +32;
 console.log(`${celsius}ºC = ${fahrenheit}ºF`)
 
+// 5
 const totalCentimos = 347
 const euros = Math.floor(totalCentimos / 100);
 const centimos = totalCentimos % 100;
 console.log(`${euros} euros y ${centimos}`);
 
+// 6
 const precioBase = 49.99;
 const iva = precioBase * 0.21;
 const precioFinal = precioBase + iva;
 console.log(`${precioFinal.toFixed(2)}€`);
 
+// 7
 const totalMinutos = 137;
 const horas = Math.floor(totalMinutos / 60);
 const minutos = totalMinutos % 60;
 console.log(`${minutos} y ${minutos}`);
 
+// 8
 const n = 4;
 console.log(Math.pow(n, 2));
 console.log(n ** 2);
 console.log(n ** 3);
 
+// 9
 let a = "rojo";
 let b = "azul";
 const temp = a;
 a = b;
 b = temp;
 console.log(a, b);
+
+// 10
+const distanciaKm = 250;
+const tiempoHoras = 2.5;
+const KmHora = 250 / 2.5;
+console.log(`Tarda ${KmHora} Km/h`)
---
+++ src/JavaScript3_3/3.js
@@ -0,0 +1,77 @@
+// 29
+const edad = 16;
+if (edad > 18) {
+    console.log(`Mayor de edad`)
+} else {
+    console.log(`Menor de edad`)
+}
+
+// 30
+const numero = 7;
+const resto = 7 % 2;
+if (resto === 0) {
+    console.log(`Es par`)
+} else {
+    console.log(`Es impar`)
+}
+
+// 31
+const n = -3
+if (n === 0) {
+    console.log(`Es 0`)
+} else if (n > 0) {
+    console.log(`Es positivo`)
+} else {
+    console.log(`Es negativo`)
+}
+
+// 32
+const nota = 4.8;
+if (nota >= 5) {
+    console.log(`Aprobado`)
+} else {
+    console.log(`Suspenso`)
+}
+
+// 33
+const a = 15;
+const b = 28;
+if (a > b) {
+    console.log(`a es mayor`)
+} else if (b > a) {
+    console.log(`b es mayor`)
+} else {
+    console.log(`Son iguales`)
+}
+
+// 34
+const temp = 15
+if (temp < 0) {
+    console.log(`Helada`)
+} else if (temp > 0 && temp <= 10) {
+    console.log(`Fría`)
+} else if (temp > 10 && temp <= 20) {
+    console.log(`Fresca`)
+} else if (temp > 20 && temp <= 30) {
+    console.log(`Agradable`)
+} else {
+    console.log(`Calurosa`)
+}
+
+// 35
+const nota = 73
+if (nota < 60) {
+    console.log(`F`)
+} else if (nota > 60 && nota <= 69) {
+    console.log(`D`)
+} else if (nota > 69 && nota <= 79) {
+    console.log(`C`)
+} else if (nota > 79 && nota <= 89) {
+    console.log(`B`)
+} else if (nota > 89 && nota <= 100){
+    console.log(`A`)
+} else {
+    console.log(`Nota no válida`)
+}
+
+// 36
